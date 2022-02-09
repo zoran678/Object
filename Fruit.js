c36a62758fbb3329e4ac4681af83ccd809f4624c
zoran678 <82155793+zoran678@users.noreply.github.com>
2022-02-09 13:14:34 +0530

    Add files via upload

---
+++ Fruit.js
@@ -0,0 +1,57 @@
+img = "";
+objects = [];
+status = "";
+
+function preload(){
+  img = loadImage('fruit.png');
+}
+
+
+function setup() {
+  canvas = createCanvas(640, 420);
+  canvas.center();
+
+  objectDetector = ml5.objectDetector('cocossd', modelLoaded);
+  document.getElementById("status").innerHTML = "Status : Detecting Objects";
+}
+function modelLoaded()
+ {
+  console.log("Model Loaded")
+  status = true;
+  objectDetector.detect(img, gotResult);
+}
+
+function gotResult(error, results) {
+  if (error) {
+    console.log(error);
+  }
+
+  console.log(results);
+  objects = results;
+}
+
+
+function draw() {
+    image(img, 0, 0, 640, 420);
+  if (status != "") {
+  	  
+    for (var i = 0; i < objects.length; i++) {
+      document.getElementById("status").innerHTML = "Status : Objects Detected";
+
+      fill("#ff0000");
+      percent = floor(objects[i].confidence * 100);
+      text(objects[i].label + " " + percent + "%", objects[i].x + 5, objects[i].y + 15);
+      noFill();
+      stroke("#ff0000");
+      rect(objects[i].x, objects[i].y, objects[i].width, objects[i].height);
+    }
+
+    
+  }
+
+
+}
+function back()
+{
+    window.location.assign("index.html");
+}
